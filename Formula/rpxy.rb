bee28cf3bae17d23a0a30e320b59e3cd702358c7
Yuval Roth <83040004+yuval-r@users.noreply.github.com>
2025-12-18 20:25:52 -0600

    Update rpxy formula: use resource block and add git-lfs dependency

--- Formula/rpxy.rb
+++ Formula/rpxy.rb
@@ -1,16 +1,24 @@
 class Rpxy < Formula
   desc "Simple and ultrafast HTTP reverse proxy in Rust"
   homepage "https://rpxy.io"
-  url "https://github.com/junkurihara/rust-rpxy.git", tag: "0.10.3"
-  version "0.10.3"
   license "Apache-2.0"
 
   depends_on "rust" => :build
+  depends_on "git-lfs"
+
+  resource "rpxy-source" do
+    url "https://github.com/junkurihara/rust-rpxy.git", tag: "0.10.3"
+  end
 
   def install
-    ENV["GIT_LFS_SKIP_SMUDGE"] = "1"
-    system "git", "submodule", "update", "--init", "--recursive"
-    system "cargo", "install", *std_cargo_args, "--root", prefix, "--path", "."
+    resource("rpxy-source").stage do
+      ENV["GIT_LFS_SKIP_SMUDGE"] = "1"
+      # Configure git to skip LFS for submodules
+      system "git", "config", "filter.lfs.smudge", "cat"
+      system "git", "config", "filter.lfs.required", "false"
+      system "git", "submodule", "update", "--init", "--recursive"
+      system "cargo", "install", *std_cargo_args, "--root", prefix, "--path", "."
+    end
   end
 
   test do
