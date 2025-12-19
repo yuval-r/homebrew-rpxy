eab0fc8114829ccebf1d5115e52940c0763ee343
Yuval Roth <83040004+yuval-r@users.noreply.github.com>
2025-12-18 20:30:52 -0600

    Add submodules: false to resource to prevent automatic submodule update

--- Formula/rpxy.rb
+++ Formula/rpxy.rb
@@ -5,19 +5,24 @@ class Rpxy < Formula
 
   depends_on "rust" => :build
 
-  # Use submodules: false to prevent automatic submodule update during download
-  # We'll handle submodules manually in install with LFS skip configured
-  url "https://github.com/junkurihara/rust-rpxy.git", tag: "0.10.3", using: :git, submodules: false
+  # Use resource to have more control over the download process
+  # submodules: false prevents automatic submodule update during download
+  resource "rpxy-source" do
+    url "https://github.com/junkurihara/rust-rpxy.git", tag: "0.10.3", revision: "deb01efad0a587a6bfba7f3868eb67a7a44cd311", using: :git, submodules: false
+  end
 
   def install
-    # Set environment variable to skip git-lfs
+    # Set environment variable to skip git-lfs BEFORE staging
     ENV["GIT_LFS_SKIP_SMUDGE"] = "1"
-    # Configure git to skip LFS for submodules
-    system "git", "config", "filter.lfs.smudge", "cat"
-    system "git", "config", "filter.lfs.required", "false"
-    # Update submodules manually with LFS skip configured
-    system "git", "submodule", "update", "--init", "--recursive"
-    system "cargo", "install", *std_cargo_args, "--root", prefix, "--path", "."
+    
+    resource("rpxy-source").stage do
+      # Configure git to skip LFS for submodules
+      system "git", "config", "filter.lfs.smudge", "cat"
+      system "git", "config", "filter.lfs.required", "false"
+      # Update submodules manually with LFS skip configured
+      system "git", "submodule", "update", "--init", "--recursive"
+      system "cargo", "install", *std_cargo_args, "--root", prefix, "--path", "."
+    end
   end
 
   test do
