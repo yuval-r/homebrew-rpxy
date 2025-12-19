33c501587c4e214b3479924919a1dd95d5a2bf8e
Yuval Roth <83040004+yuval-r@users.noreply.github.com>
2025-12-18 21:10:37 -0600

    Fix SHA256 checksum to match actual release binary

--- Formula/rpxy.rb
+++ Formula/rpxy.rb
@@ -2,7 +2,7 @@ class Rpxy < Formula
   desc "Simple and ultrafast HTTP reverse proxy in Rust"
   homepage "https://rpxy.io"
   url "https://github.com/yuval-r/homebrew-rpxy/releases/download/v0.10.3/rpxy-0.10.3-macos.tar.gz"
-  sha256 "8bb79b55b298b3ba2b178ec6ef269aa103655ddb0fd5a2a7c85301051cc682c6"
+  sha256 "3fb21af80b68b53ed19d2a0c21251d12b381ee95f65d656d687bdfd9dd392d95"
   version "0.10.3"
   license "MIT"
 
