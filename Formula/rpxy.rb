ad10739c772c735d2935826931c01fa8809fc17a
Yuval Roth <83040004+yuval-r@users.noreply.github.com>
2025-12-18 20:38:06 -0600

    Fix: Add required URL to formula (remove resource block)

--- Formula/rpxy.rb
+++ Formula/rpxy.rb
@@ -1,32 +1,23 @@
 class Rpxy < Formula
   desc "Simple and ultrafast HTTP reverse proxy in Rust"
   homepage "https://rpxy.io"
+  url "https://github.com/junkurihara/rust-rpxy.git",
+      revision: "deb01efad0a587a6bfba7f3868eb67a7a44cd311",
+      using:    :git,
+      submodules: false
   license "Apache-2.0"
 
   depends_on "rust" => :build
 
-  # Use resource to have more control over the download process
-  # Note: Homebrew automatically updates submodules during download if .gitmodules exists
-  # This happens before dependencies are installed, so git-lfs must be pre-installed
-  resource "rpxy-source" do
-    url "https://github.com/junkurihara/rust-rpxy.git",
-        revision: "deb01efad0a587a6bfba7f3868eb67a7a44cd311",
-        using:    :git,
-        submodules: false
-  end
-
   def install
-    # Set environment variable to skip git-lfs BEFORE staging
+    # Set environment variable to skip git-lfs
     ENV["GIT_LFS_SKIP_SMUDGE"] = "1"
-    
-    resource("rpxy-source").stage do
-      # Configure git to skip LFS for submodules
-      system "git", "config", "filter.lfs.smudge", "cat"
-      system "git", "config", "filter.lfs.required", "false"
-      # Update submodules manually with LFS skip configured
-      system "git", "submodule", "update", "--init", "--recursive"
-      system "cargo", "install", *std_cargo_args, "--root", prefix, "--path", "."
-    end
+    # Configure git to skip LFS for submodules
+    system "git", "config", "filter.lfs.smudge", "cat"
+    system "git", "config", "filter.lfs.required", "false"
+    # Update submodules manually with LFS skip configured
+    system "git", "submodule", "update", "--init", "--recursive"
+    system "cargo", "install", *std_cargo_args, "--root", prefix, "--path", "."
   end
 
   test do
