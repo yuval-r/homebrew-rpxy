560f66bf082b8176518a6431dd25391bfde207f5
Yuval Roth <83040004+yuval-r@users.noreply.github.com>
2025-12-18 21:35:22 -0600

    Update SHA256 for flat tarball structure

--- Formula/rpxy.rb
+++ Formula/rpxy.rb
@@ -2,7 +2,7 @@ class Rpxy < Formula
   desc "Simple and ultrafast HTTP reverse proxy in Rust"
   homepage "https://rpxy.io"
   url "https://github.com/yuval-r/homebrew-rpxy/releases/download/v0.10.3/rpxy-0.10.3-macos.tar.gz"
-  sha256 "3fb21af80b68b53ed19d2a0c21251d12b381ee95f65d656d687bdfd9dd392d95"
+  sha256 "3c9a9b5a7394430535c487a448d2dca17aa17507ce42c52087a3b3a3f25895f9"
   version "0.10.3"
   license "MIT"
 
